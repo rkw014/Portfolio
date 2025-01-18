6441a695e2b3776c9c3dd2e7794791102b27cb04
rkw014 <163924157+rkw014@users.noreply.github.com>
2025-01-18 02:29:29 -0500

    user update modification

--- user-service/src/main/java/com/rk/portfolio/user_service/controller/UserController.java
+++ user-service/src/main/java/com/rk/portfolio/user_service/controller/UserController.java
@@ -2,12 +2,13 @@ package com.rk.portfolio.user_service.controller;
 
 import com.rk.portfolio.user_service.model.User;
 import com.rk.portfolio.user_service.service.UserService;
+
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -23,26 +24,17 @@ public class UserController {
     }
 
     @PostMapping()
-    @Transactional
     public ResponseEntity<String> createUser(
             @RequestHeader("X-User-Id") String userId,
             @RequestHeader("X-User-Email") String email,
             @RequestHeader("X-User-Name") String userName,
             @RequestBody User user
     ){
-        Optional<User> curUser = userService.getUserBySub(userId);
-        if (curUser.isEmpty()){
-            User newUser = new User();
-            newUser.setSub( userId);
-            newUser.setEmail(email);
-            newUser.setUserName(userName);
-            newUser.setCreatedAt(LocalDateTime.now());
-            newUser.setUpdatedAt(LocalDateTime.now());
-            newUser.setVersion(0L);
-            User createdUser = userService.createUser(newUser);
-            return ResponseEntity.ok("OK " + createdUser.getId().toString());
+        User curUser = userService.createUser(userId,email,userName);
+        if (curUser == null){
+            return ResponseEntity.ok("OK");
         }
-        return ResponseEntity.ok("OK");
+            return ResponseEntity.ok("OK " + curUser.getId().toString());
     }
 
     @GetMapping("/{sub}")
@@ -52,24 +44,17 @@ public class UserController {
     }
 
     @PutMapping("/{sub}")
-    @Transactional
     public ResponseEntity<String> updateUser(@PathVariable String sub, @RequestBody User userDetails) {
-        Optional<User> existingUser = userService.getUserBySub(sub);
-        if (existingUser.isPresent()) {
-            User user = existingUser.get();
-            user.setUserName(userDetails.getUserName());
-            user.setEmail(userDetails.getEmail());
-            user.setUpdatedAt(LocalDateTime.now());
-            User updatedUser = userService.updateUser(user);
-            return ResponseEntity.ok("OK");
-        } else {
+        try {
+            userService.updateUser(sub, userDetails);
+        } catch (EntityNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
+        return ResponseEntity.ok("OK");
     }
 
     // Delete User
     @DeleteMapping("/{sub}")
-    @Transactional
     public ResponseEntity<Void> deleteUser(@PathVariable String sub) {
         Optional<User> existingUser = userService.getUserBySub(sub);
         if (existingUser.isPresent()) {
